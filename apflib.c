b0ba4abf6ab2fcbed37e646698178b609bb317ad
Jimi Chen <jimictw@google.com>
2025-04-18 13:10:22 +0000

    Add libapfv61 to support APFv6.1
    Flag: EXEMPT mainline
    Test: m libapf
    Change-Id: I631e1e3e315c563b294aecfc73c9adacbbe229b8

--- apflib.c
+++ apflib.c
@@ -21,21 +21,48 @@
 #undef apf_run
 #undef apf_version
 
+#define apf_run apfv61__apf_run
+#define apf_version apfv61__apf_version
+#include <v6.1/apf_interpreter.h>
+#undef apf_run
+#undef apf_version
+
 #include <next/apf_interpreter.h>
 
-#define APF_VERSION_V6 6000
+#define APF_VERSION_V2 2
+#define APF_VERSION_V4 4
+#define APF_VERSION_V6 (int)apfv6__apf_version()
+#define APF_VERSION_V61 (int)apfv61__apf_version()
+
 int apf_run_generic(int apf_version, uint32_t* program,
                     uint32_t program_len, uint32_t ram_len,
                     const uint8_t* packet, uint32_t packet_len,
                     uint32_t filter_age) {
-    if (apf_version <= 4) {
+    if (apf_version < APF_VERSION_V2) {
+        return -1;
+    }
+
+    // TODO: handle APFv2 version interpreter
+    if (apf_version <= APF_VERSION_V4) {
         return accept_packet((uint8_t*)program, program_len, ram_len, packet, packet_len,
                              filter_age >> 14);
-    } else if (apf_version == APF_VERSION_V6) {
+    }
+
+    if (apf_version < APF_VERSION_V6) {
+        return accept_packet((uint8_t*)program, program_len, ram_len, packet, packet_len,
+                             filter_age >> 14);
+    }
+
+    if (apf_version < APF_VERSION_V61) {
         return apfv6__apf_run(nullptr, program, program_len, ram_len, packet, packet_len,
                           filter_age);
-    } else {
-        return apf_run(nullptr, program, program_len, ram_len, packet, packet_len,
-                       filter_age);
     }
+
+    if (apf_version == APF_VERSION_V61) {
+        return apfv61__apf_run(nullptr, program, program_len, ram_len, packet, packet_len,
+                          filter_age);
+    }
+
+    return apf_run(nullptr, program, program_len, ram_len, packet, packet_len,
+                    filter_age);
 }
